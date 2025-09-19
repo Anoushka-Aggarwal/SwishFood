6087f40a77e9b25559a310fa2fabdbfaa501a2ba
Anoushka Aggarwal <anoushkaaggarwal@Anoushkas-MacBook-Air.local>
2025-09-19 12:09:18 +0530

    Fixing admin

--- Admin/src/assets/assets.js
+++ Admin/src/assets/assets.js
@@ -5,13 +5,14 @@ import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
-export const assets ={
-    logo,
-    add_icon,
-    order_icon,
-    profile_image,
-    upload_area,
-    parcel_icon
+export const assets = {
+  logo,
+  add_icon,
+  order_icon,
+  profile_image,
+  upload_area,
+  parcel_icon
 }
 
-export const url = 'http://localhost:4000'
+// ✅ Get backend URL from .env
+export const url = import.meta.env.VITE_BACKEND_URL;
